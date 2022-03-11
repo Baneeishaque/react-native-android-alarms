8fb3af97e4c4ec8b2fabd2158409ca94525ed7c4
Banee Ishaque K <baneeishaque@gmail.com>
2022-03-12 00:34:36 +0530

    Android Build OK

--- android/src/main/java/com/dawnchorus/alarms/RebootModule.java
+++ android/src/main/java/com/dawnchorus/alarms/RebootModule.java
@@ -2,6 +2,7 @@
 package com.dawnchorus.alarms;
 
 import android.app.Activity;
+
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
@@ -17,6 +18,7 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -40,23 +42,18 @@ public class RebootModule extends ReactContextBaseJavaModule implements Activity
 
     @Override
     public String getName() {
-      return "RebootForAlarms";
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-      Log.v("testing", "new intent");
-    }
-
-    @Override
-    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-      Log.v("testing", "onActivityResult");  
+        return "RebootForAlarms";
     }
 
     public static void sendEvent(String event) {
         WritableNativeMap params = new WritableNativeMap();
         reactContext
-            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-            .emit(event, params);
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(event, params);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.v("testing", "onActivityResult");
     }
 }
